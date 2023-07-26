82e820b8c7fa27f1d19947c17b84387e6a129e27
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-26 13:00:40 +0900

    :classディレクティブを試す

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,42 @@
+<!-- $ npm run dev -->
+<!-- JavaScriptのコードが入る -->
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+  const flag = true
+  const isActive = true
+  const isBlack = true
+  const isWhite = true
+  const isOrage = 'bgColorOrange'
+  const isYello = 'colorYello';
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+  <h1>Vue 3 入門</h1>
+  <!-- class属性 -->
+  <!-- <p class="active">v-bindでクラス属性を変更してみる。</p> -->
+  <!-- :classディレクティブの値にオブジェクトを設定。クラス名をキーに値に設定した変数をいれる。 -->
+  <!-- <p :class="{ active: flag }">class attribute active</p> -->
+  <!-- 通常のクラス定義と併用できる。 -->
+  <!-- <p class="underLine" :class="{ active: flag }">class attribute active</p> -->
+  <!-- 通常のクラスを:classディレクティブの値として複数を『''』で括り、真偽値を当ててスタイルをつけることができる。 -->
+  <!-- <p :class="{ 'underLine active': flag }">underline</p> -->
+  <!-- 変数の値の真偽値が真であるとき、『.active』のスタイルを充てる。 -->
+  <!-- <p :class="flag && 'active'">flag active color red</p> -->
+  <!-- if条件節で判定する。 -->
+  <!--<p :class="flag ? 'active' : 'underLine'">if条件節で判定する。</p>-->
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+  .active {
+    color: #f00;
+    font-weight: 900;
+  }
+  .underLine {
+    text-decoration: underline;
+  }
+  .bgColorBlack {
+    background-color: #333;
+  }
+  .colorWhite {
+    color: #fff;
+  }
 </style>
