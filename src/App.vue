d62c2cebe00a43e61e30e4f983fdfe71b0abdb57
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-28 06:09:27 +0900

    属性を変更する style, class

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,14 @@
 <!-- $ npm run dev -->
 <!-- JavaScriptのコードが入る -->
 <script setup>
-  // 変数を生成させるときにはじゅうぶん注意する。
-  let message = 'hello world'
-  const upperCase = ()=> {
-    message = message.toUpperCase()
-  }
-  upperCase();
+  // 変数を生成させるときには十分に注意する。
+  let colorRed = 'red'
+  let fontWeight = '900'
 </script>
 
 <template>
   <h1>Vue 3 入門</h1>
-  <p v-text="message"></p>
+  <p :style="{ color: colorRed, fonrWeight: fontWeight }">hello</p>
 </template>
 
 <style scoped>
