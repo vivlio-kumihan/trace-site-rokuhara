3b0144801b8949bcf0de0e67e56a9140609d8ff3
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-07-29 06:11:49 +0900

    array, object

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,93 @@
 <!-- $ npm run dev -->
 
-<!-- v-if, v-if-else, v-else -->
-<!-- v-if ディレクティブを利用することで要素単位またはブロック単位で
-      表示・非表示または表示内容を変えることができます。 -->
+<!-- 配列・オブジェクト -->
 
 <script setup>
-  const errorOne = 'エラーが発生しています。'
-  const errorTwo = ''
-  const stock = 0
-  const errorThree = false
+  const lang = ['JavaScript', 'HTML', 'CSS']
+  const users = [
+    {id: 1, name: '髙廣信之', email: 'nob@email.com', admin: true},
+    {id: 2, name: '髙廣和恵', email: 'kaz@email.com', admin: false},
+    {id: 3, name: '髙廣茉李', email: 'mari@email.com', admin: false}
+  ]
+  const nob = {
+    id: 1,
+    name: 'nobuyuki Takahiro',
+    email: 'nob@email.com',
+    admin: true
+  }
 </script>
 
 <template>
-  <h1>v-if</h1>
-  <!-- 変数`errorOne`に文字列が入っているので`true`だから -->
-  <!-- マスタッシュで変数が展開される。 -->
-  <p v-if="errorOne">{{ errorOne }}</p>
-  
-  
-  <!-- 変数`errorOne`に文字列が入ってないので`false`だから -->
-  <!-- `v-else`でマスタッシュで変数が展開される。 -->
-  <p v-if="errorTwo">{{ errorTwo }}</p>
-  <p v-else>エラーはありません。</p>
-  
-  <!-- if条件文の論理だての基本を表している。暗記。-->
-  <p v-if="stock > 5">在庫あり</p>
-  <p v-else-if="stock === 0">在庫なし</p>
-  <p v-else>在庫僅少</p>
-  
-  <h1>v-show</h1>
-  <!-- falseの場合は存在を残す。style="display: none;" --> 
-  <!-- システムに優しい --> 
-  <p v-if="errorThree">エラーが発生しています。</p>
-  <p v-show="errorThree">エラーが発生しています。</p>
+  <h1>配列</h1>
+  <!-- 配列の値を出力する。--> 
+  <p>{{ lang[0] }}</p>
+  <p>{{ lang[1] }}</p>
+  <p>{{ lang[2] }}</p>
+
+  <!-- v-forで回す。シンプル。-->
+  <p v-for="l in lang" :key="l">{{ l }}</p>
+
+  <h1>オブジェクト</h1>
+  <template v-for="(user, idx) in users" :key="user">
+    <ul>
+      <li>{{ idx }}. </li>
+      <li>{{ user.id }}</li>
+      <li>{{ user.name }}</li>
+      <li>{{ user.email }}</li>
+    </ul>
+  </template>
+
+  <template v-for="value in nob" :key="value">
+    <p>
+      {{ value }}
+    </p>
+  </template>
+
+  <ul class="key-in-obj">
+    <template v-for="(value, key) in nob" :key="value">
+      <div>
+        <dt>{{ key }}</dt>
+        <dd>{{ value }}</dd>
+      </div>
+    </template>
+  </ul>
+
+  <ul class="obj-in-arr">
+    <template>
+      <div v-for="user in users" :key="user.id">
+        <div v-for="(value, key) in user" :key="value">
+          {{ key }}: {{ value }}
+        </div>
+      </div>
+    </template>
+  </ul>
 </template>
 
 <style scoped>
   p {
     margin-bottom: 10px;
+  }
+  ul {
+    display: flex;
+  }
+
+  ul + ul {
+    margin-top: 20px;
+  }
+
+  ul.key-in-obj {
+    flex-direction: column;
+  }
+  
+  .key-in-obj dt, .key-in-obj dd {
+    margin-bottom: 10px
+  }
+
+  .key-in-obj div {
+    display: flex;
+    gap: 20px;
+  }
+  .key-in-obj div dt {
+    width: 50px;
   }
 </style>
