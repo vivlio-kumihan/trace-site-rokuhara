c3868dda3edc11edb8f9f6a2c153618029887edf
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-08-01 05:54:59 +0900

    Form, Modifiers(修飾子)

--- src/App.vue
+++ src/App.vue
@@ -1,156 +1,172 @@
 <!-- $ npm run dev -->
 
-<!-- Reactivity -->
-<!--
+<!-- Form -->
+
+<!-- ReactiveではないFormの状態 -->
+<!-- 
 <script setup>
-  // Zero
-  // クリックしても何も起こらない。
-  // count 数が増えないのは定義した count 変数が
-  // 『Reactivity』 を持っていないため。
-  const count = 0
+  const message = 'hello world'
+  const clickButton = () => {
+    console.log(message)
+  }
 </script>
+
 <template>
-  <h2>Reactivity Zero</h2>
-  <button @click="count++">Count is {{ count }}</button>
+  <h1>Form</h1>  
+  <p>{{ message }}</p>
+  <input type="text" v-model="message">
+  <div><button @click="'clickButton'">click</button></div>
 </template>
+-->
 
+<!-- ref()関数を使ってみる。
+  v-model ディレクティブを利用することで input 要素で
+  入力した値と reactive な変数を同期させることができる。
 
+  v-model ディレクティブを利用している場合に input 要素に
+  value 属性を設定してエラーになるため利用することはできない。
+  placehoder 属性を利用することはできる。-->
+<!--  
 <script setup>
-  // One-1
-  // ref()関数の設定
-
-  // ref()関数をimport
-  // ref()関数に『0』の引数を代入して変数countを初期化する。
-  // これで変数countはRectiveになる。
   import { ref } from 'vue'
-  const count = ref(0);
-</script>
-<template>
-  <h2>Reactivity One</h2>
-  <button @click="count++">Count is {{ count }}</button>
-</template>
+  const message = ref('hello world')
+  const clickButton = () => {
+    console.log(message.value)
+  }
+  </script>
+  
+  <template>
+  <h1>Form</h1>
+  <p>{{ message }}</p>
+  <input type="text" v-model="message">
+  <div><button @click="clickButton">click</button></div>
+  </template>
+-->
 
+<!-- reactive()関数を使ってみる。
+      引数にオブジェクトで設定を行う。
 
+      v-model ディレクティブを利用している場合に input 要素に
+    　value 属性を設定してエラーになるため利用することはできない。
+      placehoder 属性を利用することはできる。
+-->
+<!-- 
 <script setup>
-  // One-2
-  // ref()関数のラッパー
-
-  import { ref } from 'vue'
-  const count = ref('object')
-  // 変数countは、オブジェクトでラップされており
-  // value プロパティのみを持っている。
-  // 値を参照するにはメソッドを送信する。
-  console.log(count.value);
-</script>
-<template></template>
-
-
-<script setup>
-  // Two
-  // ref()関数に関数を定義する
-
-  // ref では引数： boolean, string, オブジェクトを取る。
-  // 例では数値を取っている。どうやって他の引数を取るのかわからない。
-  import { ref } from 'vue'
-  const count = ref(0)
-  // tmplateタグ内では順次状況によって数値は変化していった、
-  // scriptタグ内では無名関数を値にした変数を定義する必要がある。
-  const addCount = () => {
-    count.value++
-  };
-</script>
-<template>
-  <h2>Reactivity Two</h2>
-  <button @click="addCount">Count is {{ count }}</button>
-</template>
-
-
-<script setup>
-  // Three-1
-  // reactive な変数を定義する。
-  // カウントアップの動作部はtmplate内で実行する。
-  // reactive 関数
-  //   reactive では引数：オブジェクトを取る。
-
   import { reactive } from 'vue'
-  // インスタンス『state』に『count』メソッドを送信して値を得る。
-  const state = reactive({
-    count: 0
-  });
-</script>
-<template>
-  <h2>Reactivity Three-1</h2>
-  <button @click="state.count++">Count is {{ state.count }}</button>
-</template>
-
-
-<script setup>
-  // Three-2
-  // 先ほどはtemplate内で動作部を実行したが、今回はscript内で実行させる。
-  // reactive()関数で生成したインスタンスに関数を定義する
-  import { reactive } from 'vue'
-  const state = reactive({
-    count: 0
+  const form = reactive({
+    message: 'hello world'
   })
-  const addCount = () => {
-    state.count++
-  };
+  const clickButton = () => {
+    console.log(form.message)
+  }
+  </script>
+  
+  <template>
+  <h1>Form</h1>
+  <p>{{ form.message }}</p>
+  <input type="text" v-model="form.message">
+  <div><button @click="clickButton">click</button></div>
+  </template>
+-->
+
+<!-- Modifiers(修飾子)を使ってみる。-->
+<!-- v-model には修飾子を設定することができ修飾子を利用することで
+      v-model の動作を変更することができる。
+      lazy
+      trim
+      number
+-->
+
+<!-- 
+laze
+  input 要素に文字を入力すると即時に reactive な変数へ反映されていた。
+  修飾子を lazy をつけることで文字の入力毎ではなく
+  input 要素に入力後、エンターを押したら反映される。
+  この挙動は気持ちいい。
+-->
+<!-- 
+<script setup>
+  import { reactive } from 'vue'
+  const form = reactive({
+    message: 'hello world'
+  })
+  const clickButton = () => {
+    console.log(form.message)
+  }
 </script>
+
 <template>
-  <h2>Reactivity Three-2</h2>
-  <button @click="addCount">Reactive Count is {{ state.count }}</button>
+  <h1>Form</h1>
+  <p>{{ form.message }}</p>
+  <input type="text" v-model.lazy="form.message">
+  <div><button @click="clickButton">click</button></div>
 </template>
+-->
 
+<!-- 
+trim
+  input 要素の先頭や最後に空白がある場合取り除きたい場合に
+  修飾子の trim をつけることで自動で取り除いてくれる。
+  入力の段階では前後の空白は入力したまま反映されるが、
+  クリックボタンを押した瞬間に trim が作動する。
+-->
 
+<!-- 
+<script setup>
+  import { reactive } from 'vue'
+  const form = reactive({
+    message: 'hello world'
+  })
+  const clickButton = () => {
+    console.log(form.message)
+  }
+</script>
+
+<template>
+  <h1>Form</h1>
+  <p>{{ form.message }}</p>
+  <input type="text" v-model.trim="form.message">
+  <div><button @click="clickButton">click</button></div>
+</template>
+-->
+
+<!-- 
+number
+  JavaScript では input 要素に入力した値を取得すると
+  文字列として扱う。
+  数字とし取得したい場合は修飾子の number を設定することで
+  数値として取得する。
+  type属性 => number: Vue が自動で number に変換する。
+  type属性 => text: この時に有効。
+
+  v-model="form.message"
+      最初は設定通り数列が入力されている。
+      文字列入れる => string
+      数列入れる => string
+
+  v-model.number="form.message"
+      最初は設定通り数列が入力されている。
+      文字列入れる => string
+      数列入れる => number
+  とちゃんと見分けてくれる。
+  入力されたのが文字列か数列で振り分け作業ができることがわかる。
 -->
 <script setup>
-  // Five
-  // ref関数は、template内で動作部を実行することができない。
-  // ref関数で生成したインスタンスに関数を定義する
-  // ref関数を設定する
-  import { ref } from 'vue';
-  const state = ref({
-    count: 0
-  });
-  const addCount = () => {
-    state.value.count++;
+  import { reactive } from 'vue'
+  const form = reactive({
+    message: 12345
+  })
+  const clickButton = () => {
+    console.log(typeof form.message)
   };
 </script>
+
 <template>
-  <h2>Reactivity Four</h2>
-  <button @click="addCount">Ref Count is {{ state.count }}</button>
+  <h1>Form</h1>
+  <p>{{ form.message }}</p>
+  <input type="text" v-model.number="form.message">
+  <div><button @click="clickButton">click</button></div>
 </template>
 
 <style scoped>
-  p {
-    margin-bottom: 10px;
-  }
-  ul {
-    display: flex;
-  }
-
-  ul + ul {
-    margin-top: 20px;
-  }
-
-  ul.key-in-obj {
-    flex-direction: column;
-  }
-  
-  .key-in-obj dt, .key-in-obj dd {
-    margin-bottom: 10px
-  }
-
-  .key-in-obj div {
-    display: flex;
-    gap: 20px;
-  }
-  .key-in-obj div dt {
-    width: 50px;
-  }
-  button {
-    margin-bottom: 20px;
-    padding: 5px;
-    border-radius: 5px;
-  }
 </style>
