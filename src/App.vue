2d76bad3de110a74c715d586f911e68dd2760dfc
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-08-10 09:55:14 +0900

    配列とオブジェクトのおさらい

--- src/App.vue
+++ src/App.vue
@@ -3,91 +3,57 @@
 <!-- 配列・オブジェクト -->
 
 <script setup>
-  const lang = ['JavaScript', 'HTML', 'CSS']
+  const flag = true
   const users = [
-    {id: 1, name: '髙廣信之', email: 'nob@email.com', admin: true},
-    {id: 2, name: '髙廣和恵', email: 'kaz@email.com', admin: false},
-    {id: 3, name: '髙廣茉李', email: 'mari@email.com', admin: false}
-  ]
-  const nob = {
-    id: 1,
-    name: 'nobuyuki Takahiro',
-    email: 'nob@email.com',
-    admin: true
-  }
+    { id: 1, name: '髙廣信之', email: 'nob@email.com', admin: true },
+    { id: 2, name: '髙廣和恵', email: 'kaz@email.com', admin: false },
+    { id: 3, name: '髙廣茉李', email: 'mari@email.com', admin: false }
+  ];
 </script>
+<!-- 
+  # v-forは判別してる
+    - 対象が配列かオブジェクトかは自動で見分けている。 
+    - 引数には以下のような順番で設定する。
+    配列:         値  > インデックス
+    オブジェクト:  値 > キー > インデックス
 
+  # v-forは値についても判別している
+    - 対象がオブジェクトであれば、そのメソッドを充てて値を取り出せる。
+
+  # 変数のスコープ
+    - v-if, v-forで回している中がスコープ範囲。
+    - 変数はイミュータブル。
+  # 条件文を使った表示の切り替えは必須。
+-->
 <template>
-  <h1>配列</h1>
-  <!-- 配列の値を出力する。--> 
-  <p>{{ lang[0] }}</p>
-  <p>{{ lang[1] }}</p>
-  <p>{{ lang[2] }}</p>
-
-  <!-- v-forで回す。シンプル。-->
-  <p v-for="l in lang" :key="l">{{ l }}</p>
-
-  <h1>オブジェクト</h1>
-  <template v-for="(user, idx) in users" :key="user">
-    <ul>
-      <li>{{ idx }}. </li>
-      <li>{{ user.id }}</li>
-      <li>{{ user.name }}</li>
-      <li>{{ user.email }}</li>
-    </ul>
-  </template>
-
-  <template v-for="value in nob" :key="value">
-    <p>
-      {{ value }}
-    </p>
-  </template>
-
-  <ul class="key-in-obj">
-    <template v-for="(value, key) in nob" :key="value">
-      <div>
-        <dt>{{ key }}</dt>
-        <dd>{{ value }}</dd>
-      </div>
+  <ul>
+    <template v-for="(user, idx) in users" :key="user">
+      <li>index -> {{ idx }}</li>
+      <li>id -> {{ user.id }}</li>
+      <li>name -> {{ user.name }}</li>
+      <li>email -> {{ user.email }}</li>
+      <li>adimin -> {{ user.admin }}</li>
+      <li>
+        <ul class="inner">
+          <template v-for="(value, key, idx) in user" :key="key">
+            <li>{{ idx }}: {{ key }} => {{ value }}</li>
+          </template>
+        </ul>
+      </li>
     </template>
   </ul>
-
-  <ul class="obj-in-arr">
-    <template>
-      <div v-for="user in users" :key="user.id">
-        <div v-for="(value, key) in user" :key="value">
-          {{ key }}: {{ value }}
-        </div>
-      </div>
+  <ul class="judge">
+    <template v-for="user in users" :key="user">
+      <template v-if="user.admin !== true">
+        <template v-for="(value, key, idx) in user" :key="key">
+          <li>{{ idx }}: {{ key }} => {{ value }}</li>
+        </template>
+      </template>
     </template>
   </ul>
 </template>
 
-<style scoped>
-  p {
-    margin-bottom: 10px;
-  }
-  ul {
-    display: flex;
-  }
-
-  ul + ul {
-    margin-top: 20px;
-  }
-
-  ul.key-in-obj {
-    flex-direction: column;
-  }
-  
-  .key-in-obj dt, .key-in-obj dd {
-    margin-bottom: 10px
-  }
-
-  .key-in-obj div {
-    display: flex;
-    gap: 20px;
-  }
-  .key-in-obj div dt {
-    width: 50px;
-  }
+<style>
+  .inner li { color: blue; }
+  .judge li { color: green; }
 </style>
